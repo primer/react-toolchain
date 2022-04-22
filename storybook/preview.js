51156b8f57d052970e0ae7b84fafafb08aeef3ed
Siddharth Kshetrapal <siddharthkp@github.com>
2022-04-22 16:20:39 +0200

    use decorator from primer/react#2044

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,3 +1,4 @@
-import { ThemeDecorator } from '@primer/react/lib-esm/ThemeDecorator';
+import { withThemeDecorator, toolbarTypes } from '@primer/react/lib-esm/utils/story-helpers';
 
-export const decorators = [ThemeDecorator];
+export const globalTypes = toolbarTypes;
+export const decorators = [withThemeDecorator];
