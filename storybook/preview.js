e952cbf62586857a23b7231fa2b805f7d7110b96
Siddharth Kshetrapal <siddharthkp@github.com>
2022-04-25 14:26:42 +0200

    disable shortcuts

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,5 +1,12 @@
+import { addons } from '@storybook/addons';
 import { withThemeProvider, toolbarTypes } from '@primer/react/lib-esm/utils/story-helpers';
 import { withPerformance } from 'storybook-addon-performance';
 
 export const globalTypes = toolbarTypes;
 export const decorators = [withThemeProvider, withPerformance];
+
+addons.setConfig({
+  // Some stories may set up keyboard event handlers, which can be interfered
+  // with by these keyboard shortcuts.
+  enableShortcuts: false
+});
